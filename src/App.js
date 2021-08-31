bb845925839c24b2cb73f54916bb6d15db4d1b9b
this-is-pooja <poojasoliwal2222@gmail.com>
2021-08-31 20:07:21 +0530

    Music-App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Player from "./com/Player";
 
 function App() {
+  const [songs] = useState([
+    {
+      title: "Titaliya",
+      artist: "Hardy Sandhu",
+      img_src: "./images/titaliya.jpg",
+      src: "./songs/Titliaan Warga.mp3",
+    },
+    {
+      title: "Nach Meri Rani",
+      artist: "Yo Yo Honey Singh",
+      img_src: "./images/nach-meri-rani.jpg",
+      src: "./songs/Naach Meri Rani.mp3",
+    },
+    {
+      title: "Care Ni Karda",
+      artist: "Yo Yo Honey Singh",
+      img_src: "./images/care-ni-karda.jpg",
+      src: "./songs/Care Ni Karda.mp3",
+    },
+    {
+      title: "Burj Khalifa",
+      artist: "Shashi",
+      img_src: "./images/burjkalifa.jpg",
+      src: "./songs/BurjKhalifa.mp3",
+    },
+    {
+      title: "Tango Del Fuego",
+      artist: "Parov Stelar",
+      img_src: "./images/ParovStelarGeorgiaGibbs-TangoDelFuego.jpg",
+      src: "./songs/ParovStelarGeorgiaGibbs-TangoDelFuego.mp3",
+    },
+    {
+      title: "Take You Dancing",
+      artist: "Jason Derulo",
+      img_src: "./images/JasonDerulo-TakeYouDancing.jpg",
+      src: "./songs/JasonDerulo-TakeYouDancing.mp3",
+    },
+    {
+      title: "Daisy",
+      artist: "Ashnikko",
+      img_src: "./images/Ashnikko-Daisy.jpg",
+      src: "./songs/Ashnikko-Daisy.mp3",
+    },
+    {
+      title: "Dolly Song",
+      artist: "Partz Grimbad",
+      img_src: "./images/PatzGrimbard-DollySong.jpg",
+      src: "./songs/PatzGrimbard-DollySong.mp3",
+    },
+  ]);
+
+  const [currentSongIndex, setCurrentSongIndex] = useState(0);
+  const [NextSongIndex, setNextSongIndex] = useState(0);
+
+  useEffect(() => {
+    setNextSongIndex(() => {
+      if (currentSongIndex + 1 > songs.length - 1) {
+        return 0;
+      } else {
+        return currentSongIndex + 1;
+      }
+    });
+  }, [currentSongIndex, songs.length]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Player
+        currentSongIndex={currentSongIndex}
+        setCurrentSongIndex={setCurrentSongIndex}
+        NextSongIndex={NextSongIndex}
+        songs={songs}
+      />
     </div>
   );
 }
---
+++ src/com/Controls.js
@@ -0,0 +1,21 @@
+import { faBackward, faForward, faPause, faPlay } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import React from 'react'
+
+function Controls(props) {
+    return (
+        <div className="c-player--controls">
+            <button className="skip-btn" onClick={()=>props.SkipSong(false)}>
+                <FontAwesomeIcon icon={faBackward}/>
+            </button>
+            <button className="play-btn"onClick={()=>props.setIsPlaying(!props.isPlaying)}>
+                <FontAwesomeIcon icon={props.isPlaying?faPause:faPlay}/>
+            </button>
+            <button className="skip-btn"onClick={()=>props.SkipSong()}>
+                <FontAwesomeIcon icon={faForward}/>
+            </button>
+        </div>
+    )
+}
+
+export default Controls
